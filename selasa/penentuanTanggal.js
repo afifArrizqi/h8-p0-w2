d12cc98e82326c168b9ec4e7f043c276161cbc69
pippop21 <afifar38@gmail.com>
2020-02-29 08:45:45 +0700

    revisi penentuanTanggal.js

--- selasa/penentuanTanggal.js
+++ selasa/penentuanTanggal.js
@@ -1,5 +1,5 @@
-var tanggal = 21;
-var bulan = 1;
+var tanggal = 30;
+var bulan = 6;
 var tahun = 1945;
 var tanggalMax;
 var tahunCheck = tahun > 1900 && tahun < 2200;
@@ -7,61 +7,74 @@ var tahunCheck = tahun > 1900 && tahun < 2200;
 switch (bulan) {
 	case 1:
 		bulan = 'Januari';
+		console.log('Bulan   : ' + bulan);
 		tanggalMax = 31;
 		break;
 	case 2:
 		bulan = 'Februari';
+		console.log('Bulan   : ' + bulan);
 		tahun % 4 === 0 ? (tanggalMax = 29) : (tanggalMax = 28);
 		break;
 	case 3:
 		bulan = 'Maret';
+		console.log('Bulan   : ' + bulan);
 		tanggalMax = 31;
 		break;
 	case 4:
 		bulan = 'April';
+		console.log('Bulan   : ' + bulan);
 		tanggalMax = 30;
 		break;
 	case 5:
 		bulan = 'Mei';
+		console.log('Bulan   : ' + bulan);
 		tanggalMax = 31;
 		break;
 	case 6:
 		bulan = 'Juni';
+		console.log('Bulan   : ' + bulan);
 		tanggalMax = 30;
 		break;
 	case 7:
 		bulan = 'Juli';
+		console.log('Bulan   : ' + bulan);
 		tanggalMax = 31;
 		break;
 	case 8:
 		bulan = 'Agustus';
+		console.log('Bulan   : ' + bulan);
 		tanggalMax = 31;
 		break;
 	case 9:
 		bulan = 'September';
+		console.log('Bulan   : ' + bulan);
 		tanggalMax = 30;
 		break;
 	case 10:
 		bulan = 'Oktober';
+		console.log('Bulan   : ' + bulan);
 		tanggalMax = 31;
 		break;
 	case 11:
 		bulan = 'November';
+		console.log('Bulan   : ' + bulan);
 		tanggalMax = 30;
 		break;
 	case 12:
 		bulan = 'Desember';
+		console.log('Bulan   : ' + bulan);
 		tanggalMax = 31;
 		break;
 	default:
 		console.log('bulan error');
 		bulan = undefined;
+		tanggalMax = 31;
 		break;
 }
 
 switch (true) {
 	case tanggal > 0 && tanggal <= tanggalMax:
-		tanggal;
+		console.log('Tanggal : ' + tanggal);
 		break;
 	default:
 		console.log('tanggal error');
@@ -71,7 +84,7 @@ switch (true) {
 
 switch (true) {
 	case tahunCheck:
-		tahun;
+		console.log('Tahun   : ' + tahun);
 		break;
 	default:
 		console.log('tahun error');
